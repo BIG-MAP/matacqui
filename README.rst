6e791ddaf68a6b0309e1388dab314e801278fcd2
Martin Uhrin <martin.uhrin@gmail.com>
2021-01-30 18:01:25 +0100

    Voila version

--- README.rst
+++ README.rst
@@ -4,6 +4,6 @@ Materials Acquisition Form for the BIG-MAP consortium
 To launch the acquisition form click below:
 
 .. image:: https://mybinder.org/badge_logo.svg
- :target: https://mybinder.org/v2/gh/BIG-MAP/matacqui/develop?filepath=notebooks%2Facquisition_form.ipynb
+ :target: https://mybinder.org/v2/gh/BIG-MAP/matacqui.git/voila?urlpath=voila%2Frender%2Fnotebooks%2Facquisition_form.ipynb
 
 By Eibar Flores and Martin Uhrin
--- setup.py
+++ setup.py
@@ -9,42 +9,46 @@ about = {}
 with open('matacqui/version.py') as f:
     exec(f.read(), about)  # pylint: disable=exec-used
 
-setup(
-    name='matacqui',
-    version=about['__version__'],
-    description='Materials Acquisition Form for BIG-MAP',
-    long_description=open('README.rst').read(),
-    url='https://github.com/muhrin/milad.git',
-    author='Eibar Flores',
-    author_email='eibfl@dtu.dk',
-    license=__license__,
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    keywords='materials acquisition',
-    install_requires=[
-        'reportlab', 'mincepy', 'dnspython', 'jupyter', 'notebook'
-    ],
-    extras_require={
-        'dev': [
-            'ipython',
-            'pip',
-            'pytest~=5.4',
-            'pytest-cov',
-            'pre-commit~=2.2',
-            'prospector',
-            'pylint==2.5.2',
-            'twine',
-            'yapf',
-        ],
-    },
-    packages=['matacqui', ],
-    include_package_data=True,
-    test_suite='test',
-    entry_points={
-        'mincepy.plugins.types': ['matacqui_types = matacqui.provides:get_mince_types'],
-    }
-)
+setup(name='matacqui',
+      version=about['__version__'],
+      description='Materials Acquisition Form for BIG-MAP',
+      long_description=open('README.rst').read(),
+      url='https://github.com/muhrin/milad.git',
+      author='Eibar Flores',
+      author_email='eibfl@dtu.dk',
+      license=__license__,
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      keywords='materials acquisition',
+      install_requires=[
+          'reportlab',
+          'mincepy',
+          'dnspython',
+          'jupyterlab',
+          'voila',
+      ],
+      extras_require={
+          'dev': [
+              'ipython',
+              'pip',
+              'pytest~=5.4',
+              'pytest-cov',
+              'pre-commit~=2.2',
+              'prospector',
+              'pylint==2.5.2',
+              'twine',
+              'yapf',
+          ],
+      },
+      packages=[
+          'matacqui',
+      ],
+      include_package_data=True,
+      test_suite='test',
+      entry_points={
+          'mincepy.plugins.types': ['matacqui_types = matacqui.provides:get_mince_types'],
+      })
